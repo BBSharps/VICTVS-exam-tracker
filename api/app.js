8e446bc6559f8405a9503005f7f80aa2f0415cc1
BBSharps <billybriarsharps@gmail.com>
2023-04-24 12:37:06 +0100

    add /api/candidtes and tests

--- api/app.js
+++ api/app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const { getExams } = require("./controllers/exams");
+const { getCandidates } = require("./controllers/candidates");
 
 const app = express();
 app.use(express.json());
 
 app.get("/api/exams", getExams);
+app.get("/api/candidates", getCandidates);
 
 module.exports = app;
