0de9f9fd1202ab9e8fd053fffce9b205e4a563fd
BBSharps <billybriarsharps@gmail.com>
2023-04-25 09:39:08 +0100

    add cors

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express = require("express");
 const { getExams } = require("./api/controllers/exams");
 const { getCandidates } = require("./api/controllers/candidates");
 const { getInstructions } = require("./api/controllers/instructions");
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.get("/api", getInstructions);
